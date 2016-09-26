b110c01a2c0492da0efdef95958e857a200e3741
hankypanky <menshakov.artem@gamil.com>
2016-09-26 15:28:33 +0300

    finish

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -33,4 +33,11 @@ export class HeroDetailComponent implements OnInit {
         window.history.back();
     }
 
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(this.goBack);
+    }
+
+
+
 }
